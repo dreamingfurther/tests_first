6018fa60f9d1b283b19c7c4c6660998e278bf227
dreamingfurther <dreamingfurther@gmail.com>
2014-12-11 23:59:23 -0500

    tests pass

--- car_spec.rb
+++ car_spec.rb
@@ -3,7 +3,7 @@ require_relative './spec_helper'
 describe Car do
   describe '#start' do
     it 'sets the car as running' do
-      expect(Car.start).to eq 'running'
+      expect(Car.new.start).to eq 'running'
     end
   end
 end
