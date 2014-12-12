eea0755812f68bd2b7080d1204afd1046fe47aff
dreamingfurther <dreamingfurther@gmail.com>
2014-12-12 06:42:40 -0500

    tests passing

--- car.rb
+++ car.rb
@@ -9,5 +9,15 @@ class Car
     @status = 'running'
   end
 
+  def highbeams
+    if status == 'running' && lights == 'on'
+      @lights = 'highbeam'
+    elsif status == 'running' && lights == 'highbeam'
+      @lights = 'on'
+    else
+      'cannot turn on while car is off'
+    end
+  end
+
   attr_accessor :status, :lights
 end
